f92212b727c28066277d579efa2c95757474f79f
RENAUD Brevin <renaudbrevin@gmail.com>
2025-01-08 11:59:11 +0100

    Tracklist changement with vinyle

--- script/selectTrack.js
+++ script/selectTrack.js
@@ -1,36 +1,101 @@
+const tracksName = [
+    "Ethereal",
+    "Timeless Echoes",
+    "Celestial Void",
+    "Resonance",
+    "Astral Bloom",
+    "Obsidian",
+    "Transient Waves",
+    "Aurora",
+    "Infinite",
+    "Phantom Realm",
+    "Eclipse Reverie",
+    "Our End",
+    "Serenity"
+];
+
+
 const tracklist = [];
 for (let i = 1; i <= 12; i++) {
-  const track = {
-    title: `Track ${i}`,
-    duration: `${Math.floor(Math.random() * 4) + 1}:${Math.floor(Math.random() * 60).toString().padStart(2, '0')}`,
-    file: `../music/Track${i}.mp3`,
-    style: "Melodic House/Techno",
-  };
-  tracklist.push(track);
+    const track = {
+        title: tracksName[i],
+        duration: `${Math.floor(Math.random() * 4) + 1}:${Math.floor(Math.random() * 60).toString().padStart(2, '0')}`,
+        file: `../music/Eternity.mp3`,
+        style: "Melodic House/Techno",
+    };
+    tracklist.push(track);
 }
 
 let currentTrackIndex = 0;
 let rotation = 0;
 
 const vinyle = document.getElementById("vinyle-img");
-const audio = document.getElementById("audio");
+const audiotrack = document.getElementById("audio");
 const trackTitle = document.getElementById("track-title");
 const trackTime = document.getElementById("track-time");
 const trackStyle = document.getElementById("track-style");
 
-vinyle.addEventListener("click", () => {
-  // Rotate the vinyl
-  rotation += 30;
-  vinyle.style.transform = `rotate(${rotation}deg) translate(-45%, 0)`;
+let isDragging = false;
+let startY;
 
-  // Update track
-  currentTrackIndex = (currentTrackIndex + 1) % tracklist.length;
-  const track = tracklist[currentTrackIndex];
-  trackTitle.textContent = `${currentTrackIndex + 1} - ${track.title}`;
-  trackTime.textContent = `Time : ${track.duration}`;
-  trackStyle.textContent = track.style;
-
-  // Change audio source and play
-  audio.src = track.file;
-  audio.play();
+vinyle.addEventListener('mousedown', (e) => {
+    isDragging = true;
+    startY = e.clientY;
 });
+
+document.addEventListener('mousemove', (e) => {
+    if (!isDragging) return;
+
+    const deltaY = e.clientY - startY;
+
+    if (deltaY > 50) {
+        nextTrack();
+        isDragging = false;
+    } else if (deltaY < -50) {
+        previousTrack();
+        isDragging = false;
+    }
+});
+
+document.addEventListener('mouseup', () => {
+    isDragging = false;
+});
+
+function nextTrack() {
+    changeTrack("next")
+}
+
+function previousTrack() {
+    changeTrack("previous")
+}
+
+function changeTrack(direction) {
+    direction === "next" ? rotation += 30 : rotation -= 30;
+    vinyle.style.transform = `translate(-45%, 0) rotate(${rotation}deg)`;
+
+    if (direction === "next") {
+        currentTrackIndex = (currentTrackIndex + 1) % tracklist.length;
+    } else {
+        currentTrackIndex = (currentTrackIndex - 1 + tracklist.length) % tracklist.length;
+    }
+
+    updateTrack(currentTrackIndex);
+    stopTrack();
+}
+
+function updateTrack(currentTrackIndex) {
+    const track = tracklist[currentTrackIndex];
+    trackTitle.textContent = `${currentTrackIndex + 1} - ${track.title}`;
+    trackTime.textContent = `Time : ${track.duration}`;
+    trackStyle.textContent = track.style;
+
+    audiotrack.src = track.file;
+}
+
+function stopTrack() {
+    playBtn.classList.remove('pause');
+    bars.forEach(bar => bar.style.animationPlayState = 'paused');
+    isPlaying = false;
+}
+
+updateTrack(0)
